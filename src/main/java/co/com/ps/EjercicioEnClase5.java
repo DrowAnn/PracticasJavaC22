050aa25048160fdc7b85f1dbeb773cc0900d4757
DrowAnn <rs19990801@gmail.com>
2024-02-27 12:35:20 -0500

    Actividad 5: Adicion de la linea de programacion funcional

--- src/main/java/co/com/ps/EjercicioEnClase5.java
+++ src/main/java/co/com/ps/EjercicioEnClase5.java
@@ -9,6 +9,7 @@ public class EjercicioEnClase5 {
 
     public void palabrasMasCinco() {
 
+        //Metodo tradicional
         List<String> listaPalabras = Arrays.asList("Java", "Stream", "Ejemplo", "Programacion", "Funcional");
         List<String> listaFiltrada = new ArrayList<>();
 
@@ -18,8 +19,13 @@ public class EjercicioEnClase5 {
             }
         }
 
-        System.out.println("Las palabras con más de 5 letras en el arreglo son:");
+        System.out.println("(Tradicional) Las palabras con más de 5 letras en el arreglo son:");
         listaFiltrada.stream().forEach(System.out::println);
+        System.out.println(" ");
+
+        //Programacion Funcional
+        System.out.println("(Funcional) Las palabras con más de 5 letras en el arreglo son:");
+        listaPalabras.stream().filter(palabra -> palabra.length() > 5).forEach(System.out::println);
 
     }
 
